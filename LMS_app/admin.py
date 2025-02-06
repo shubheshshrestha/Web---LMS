ec15a54150cf6e4aaafc0b71bce480d13966a572
shubheshshrestha <shubheshshrestha@gmail.com>
2025-02-06 23:11:51 +0545

    + CRUD for Admins/Librarian

--- LMS_app/admin.py
+++ LMS_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Book, BookType, BorrowingRecord, Member, IsMember
+from django.contrib.auth.models import Group, User
 # Register your models here.
+
+# register Django models with the Django admin interface.
+admin.site.register(Book)   # This allow us to manage those models (e.g., create, read, update, delete records) through the admin dashboard.
+admin.site.register(BookType)
+admin.site.register(BorrowingRecord)
+admin.site.register(Member)
+
+
